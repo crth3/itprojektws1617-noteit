f4c26f653ebe48bac14ad7db4897666f6bb94a72
ChristianUlrichRoth <cr078@hdm-stuttgart.de>
2016-11-10 00:13:55 +0100

    DBConnection angepasst - Datenbankverbindung steht!!!

--- noteit/src/de/hdm/itprojekt/noteit/server/db/DBConnection.java
+++ noteit/src/de/hdm/itprojekt/noteit/server/db/DBConnection.java
@@ -2,102 +2,43 @@ package de.hdm.itprojekt.noteit.server.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
-import com.google.appengine.api.rdbms.AppEngineDriver;
+import com.google.appengine.api.utils.SystemProperty;
 
+/**
+ * @author Thies
+ */
 public class DBConnection {
-	
-		
-		/**
-		 * This class enables the connection to the mySQL database
-		 * TODO: Add more explanations 
-		 */
-				
-			/**
-			 * private and static instance of DBConnection
-			 * the volatile modifier is needed to prevent errors caused by
-			 * the Java Memory Model
-			 */
-			private static volatile DBConnection instance = null;
-			private static volatile Connection con = null;
-		
-			// Local Access Variables
-			// private static final String localURL = "jdbc:mysql://localhost:8889/itproject";
-			// private static final String driverLocal = "com.mysql.jdbc.Driver";
-			// Google Access Variables
-			
-			// Deployment URL
-			//private static final String googleURL = "jdbc:google:rdbms://projektname-groupfive:dbname/projektname?user=root&password=hdmgroupfive";
-			
-			// Local Dev URL
-		private static final String googleURL = "jdbc:mysql://104.154.134.231:3306/projekt-tidy-cortex-148515?user=root&password=noteit";
-			
-			
-			
-			/**
-			 * Private Constructor to prevent the creation of new objects
-			 * via "new"
-			 */
-			private DBConnection() {
-				try {
-					// Load Driver
-					// Class.forName(googleDriver);
-					 DriverManager.registerDriver(new AppEngineDriver());
-				} //catch (ClassNotFoundException e) {
-					//e.printStackTrace();
-					catch (SQLException error) {
-					instance = null;
-				}
-			}
-			
-			 /** 
-			  * Establish DB Connection
-			  * 
-			  * @return con
-			  */
-			private Connection createConnection() {
-				con = null;
-				try{
-					con = DriverManager.getConnection(googleURL);
-					// con = DriverManager.getConnection(localURL, user, password);
-				} catch (SQLException e) {
-					e.printStackTrace();
-					System.out.println("Connection Error");
-				}
-				return con;
-			}
-			
-			/**
-			 * Synchronized double-checked locking singleton
-			 * Synchronization is needed to handle multithreading
-			 * double-checking is implemented for perfomance reasons
-			 */
-			// Method to create new Singleton Connections
-//			public static Connection connection() {
-//				if(instance == null) {
-//					synchronized(DBConnection.class) {
-//						if(instance == null)
-//							instance = new DBConnection();
-//						
-//					}
-//					
-//					
-//				} return instance.createConnection();
-//			} 
-			
-			public static Connection connection() {
-			if(instance == null || con == null) {
-				synchronized(DBConnection.class) {
-					if(instance == null)
-						instance = new DBConnection();
-					
-				}
-				con = instance.createConnection();
-				
-			}
-			return con;
-		
-		} 
-	}	
-	
+
+    private static Connection con = null;
+
+    private static String googleUrl = "jdbc:google:mysql://noteit-id:noteit-db/noteit?user=root";
+    private static String localUrl = "jdbc:mysql://104.199.44.232:3306/noteit-db?user=root&password=noteit";
+    
+    public static Connection connection() {
+        if (con == null) {
+            String url = null;
+            try {
+                if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+                    // Load the class that provides the new
+                    // "jdbc:google:mysql://" prefix.
+                    Class.forName("com.mysql.jdbc.GoogleDriver");
+                    url = googleUrl;
+                } else {
+                    // Local MySQL instance to use during development.
+                    Class.forName("com.mysql.jdbc.Driver");
+                    url = localUrl;
+                }
+                System.out.println("getConnection: " + url);
+                con = DriverManager.getConnection(url);
+                
+            } catch (Exception e) {
+                con = null;
+                e.printStackTrace();
+            }
+        }
+
+        return con;
+    }
+
+}
