47fab0573b10dfcdc53a77d129bca17f0b8f7eae
Maik Zimmermann <mz059@hdm-stuttgart.de>
2016-11-18 10:59:24 +0100

    Added Report Generator

---
+++ noteit/src/de/hdm/itprojekt/noteit/server/ReportAdminImpl.java
@@ -0,0 +1,24 @@
+package de.hdm.itprojekt.noteit.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import de.hdm.itprojekt.noteit.shared.ReportAdmin;
+
+/**
+ * 
+ * @author maikzimmermann
+ *
+ */
+public class ReportAdminImpl extends RemoteServiceServlet 
+implements ReportAdmin {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void init() {
+		
+	}
+
+}
---
+++ noteit/src/de/hdm/itprojekt/noteit/shared/ReportAdminAsync.java
@@ -0,0 +1,9 @@
+package de.hdm.itprojekt.noteit.shared;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface ReportAdminAsync {
+
+	void init(AsyncCallback<Void> callback);
+
+}
