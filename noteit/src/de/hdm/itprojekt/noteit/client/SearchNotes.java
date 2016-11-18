b9b5fac66793dbccf68dd917b7f1cd3f5a090ec6
daniel-meier <dm077@hdm-stuttgart.de>
2016-11-18 15:24:31 +0100

    KLassen CreateNote, CreateNotebook, SearchNotebooks, SearchNotes angelegt

---
+++ noteit/src/de/hdm/itprojekt/noteit/client/SearchNotes.java
@@ -0,0 +1,11 @@
+package de.hdm.itprojekt.noteit.client;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class SearchNotes extends VerticalPanel {
+
+	public void onLoad() {
+		
+	}
+	
+}
