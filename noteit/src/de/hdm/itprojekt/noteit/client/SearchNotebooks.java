1e164dc569a9ae81ad2d1eb602dd7560d526754f
KimIshola <ki004@hdm-stuttgart.de>
2016-11-21 00:24:40 +0100

    Label und Button hinzugefügt

--- noteit/src/de/hdm/itprojekt/noteit/client/SearchNotebooks.java
+++ noteit/src/de/hdm/itprojekt/noteit/client/SearchNotebooks.java
@@ -1,10 +1,22 @@
 package de.hdm.itprojekt.noteit.client;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SearchNotebooks extends VerticalPanel {
 	
 	public void onLoad() {
+		
+		HorizontalPanel searchPanel = new HorizontalPanel();
+		Label searchNotizbuch = new Label("Notizbuch suchen");
+		Label suchen = new Label("Suche nach Titel:");
+		Button abbrechen = new Button("abbrechen");
+		searchPanel.add(searchNotizbuch);
+		searchPanel.add(suchen);
+		searchPanel.add(abbrechen);
+		
 		
 	}
 	
--- noteit/src/de/hdm/itprojekt/noteit/client/SearchNotes.java
+++ noteit/src/de/hdm/itprojekt/noteit/client/SearchNotes.java
@@ -1,10 +1,23 @@
 package de.hdm.itprojekt.noteit.client;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SearchNotes extends VerticalPanel {
 
 	public void onLoad() {
+		
+		HorizontalPanel searchPanel = new HorizontalPanel();
+		Label searchNotiz = new Label("Notiz suchen");
+		Label suchen = new Label("Suche nach Titel:");
+		Label inhalt = new Label("Suche nach Inhalt:");
+		Button abbrechen = new Button("abbrechen");
+		searchPanel.add(searchNotiz);
+		searchPanel.add(suchen);
+		searchPanel.add(inhalt);
+		searchPanel.add(abbrechen);
 		
 	}
 	
