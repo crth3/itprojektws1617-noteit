86199785b23baed425e38b9ad92937e9905e518e
ChristianUlrichRoth <cr078@hdm-stuttgart.de>
2016-12-13 14:37:52 +0100

    placeholder

--- noteit/src/de/hdm/itprojekt/noteit/client/report/NotesByKeyword.java
+++ noteit/src/de/hdm/itprojekt/noteit/client/report/NotesByKeyword.java
@@ -10,35 +10,35 @@ import de.hdm.itprojekt.noteit.shared.ReportAdminAsync;
 import de.hdm.itprojekt.noteit.shared.report.ReportSimple;
 import de.hdm.itprojekt.noteit.shared.report.HTMLReportWriter;
 
-public class NotesSharingInformation extends VerticalPanel{
+public class NotesByKeyword extends VerticalPanel{
 	
 private ReportAdminAsync rpAdmin = null;
 	
-	private  Label lblNotesSharingInformation;
+	private  Label lblNotesByKeyword;
 
-	public NotesSharingInformation() {
+	public NotesByKeyword() {
 		
-		rpAdmin = ClientsideSettings.getReportService();
-		
-		lblNotesSharingInformation = new Label("All Notes with Sharing Information");
-		add(lblNotesSharingInformation);
-		rpAdmin.createReportNotesGeneralInformation(new AsyncCallback<ReportSimple>() {
-
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onSuccess(ReportSimple result) {
-			
-				HTMLReportWriter hTMLReportWriter = new HTMLReportWriter();
-				final ReportSimple report = result;
-					hTMLReportWriter.process(report);
-				add(new HTML(hTMLReportWriter.getReportText()));
-			}
-		});;
+//		rpAdmin = ClientsideSettings.getReportService();
+//		
+//		lblNotesByKeyword = new Label("All notes by keyword");
+//		add(lblNotesByKeyword);
+//		rpAdmin.createReportNotesByKeyword(new AsyncCallback<NotesByKeyword>() {
+//
+//			@Override
+//			public void onFailure(Throwable caught) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//			@Override
+//			public void onSuccess(ReportSimple result) {
+//			
+//				HTMLReportWriter hTMLReportWriter = new HTMLReportWriter();
+//				final ReportSimple report = result;
+//					hTMLReportWriter.process(report);
+//				add(new HTML(hTMLReportWriter.getReportText()));
+//			}
+//		});;
 	
 			
 	}
