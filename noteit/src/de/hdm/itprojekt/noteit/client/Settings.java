41e3819a9b207fcaa43eb612d2c24d7714d6bdfb
KimIshola <ki004@hdm-stuttgart.de>
2016-12-26 13:09:43 +0100

    Klasse gesäubert

--- noteit/src/de/hdm/itprojekt/noteit/client/Settings.java
+++ noteit/src/de/hdm/itprojekt/noteit/client/Settings.java
@@ -15,22 +15,23 @@ import de.hdm.itprojekt.noteit.shared.NotesAdministration;
 import de.hdm.itprojekt.noteit.shared.NotesAdministrationAsync;
 import de.hdm.itprojekt.noteit.shared.bo.User;
 
-public class Settings extends DialogBox{
-	
+public class Settings extends DialogBox {
+
 	private final static NotesAdministrationAsync notesAdmin = GWT.create(NotesAdministration.class);
 
 	VerticalPanel vpSettingsPanel = new VerticalPanel();
-	
-	User currentUser = new User ();
-	
+
+	User currentUser = new User();
+
 	/**
 	 * Konstruktor mit Userobjekt als Übergabeparameter
+	 * 
 	 * @param user
 	 */
-	public Settings (User user){
+	public Settings(User user) {
 		this.currentUser = Homepage.getCurrentUser();
 	}
-	
+
 	/**
 	 * Create the Label
 	 */
@@ -38,36 +39,33 @@ public class Settings extends DialogBox{
 	Label lbLastName = new Label("Nachname");
 	Label lbEmail = new Label("Email");
 	Label lbAusgabeEmail = new Label("");
-	
+
 	/**
 	 * Create the Textbox
 	 */
 	TextBox tbFirstName = new TextBox();
 	TextBox tbLastName = new TextBox();
-	
+
 	/**
 	 * Create the Panel
 	 */
 	HorizontalPanel hpButtonPanel = new HorizontalPanel();
-	
+
 	/**
 	 * Create the Button
 	 */
 	Button btnAbrrechenButton = new Button("Abbrechen");
-	Button btnSichernButton = new Button("Sichern"); 
-	
-	
-	public void onLoad(){
-		
-	//currentUser.setFirstName("Kim");
-		
-		
-		
+	Button btnSichernButton = new Button("Sichern");
+
+	public void onLoad() {
+
+		// currentUser.setFirstName("Kim");
+
 		setAutoHideEnabled(true);
 		setGlassEnabled(true);
 		setText("Profil bearbeiten");
 		center();
-	
+
 		vpSettingsPanel.add(lbFirstName);
 		vpSettingsPanel.add(tbFirstName);
 		vpSettingsPanel.add(lbLastName);
@@ -77,48 +75,47 @@ public class Settings extends DialogBox{
 		vpSettingsPanel.add(hpButtonPanel);
 		hpButtonPanel.add(btnAbrrechenButton);
 		hpButtonPanel.add(btnSichernButton);
-		
+
 		vpSettingsPanel.setSpacing(40);
 		setWidget(vpSettingsPanel);
-		
+
 		tbFirstName.setText(currentUser.getFirstName());
 		tbLastName.setText(currentUser.getLastName());
 		lbAusgabeEmail.setText(currentUser.getMail());
-		
+
 		btnAbrrechenButton.addClickHandler(new ClickHandler() {
-			
+
 			@Override
 			public void onClick(ClickEvent event) {
 				Settings.this.hide();
-				
+
 			}
 		});
-		
+
 		btnSichernButton.addClickHandler(new ClickHandler() {
-			
+
 			@Override
 			public void onClick(ClickEvent event) {
-				notesAdmin.updateUser(currentUser.getId(), currentUser.getMail(), 
-						currentUser.getFirstName(), currentUser.getLastName(),
-						new AsyncCallback<Void>() {
-							
+				notesAdmin.updateUser(currentUser.getId(), currentUser.getMail(), currentUser.getFirstName(),
+						currentUser.getLastName(), new AsyncCallback<Void>() {
+
 							@Override
 							public void onSuccess(Void result) {
-								//VerticalPanel homepage = new Homepage();
+								// VerticalPanel homepage = new Homepage();
 								Noteit.setWelcomeName(tbFirstName.getText());
 								currentUser.setFirstName(tbFirstName.getText());
 								Settings.this.hide();
 								currentUser.setFirstName(tbFirstName.getText());
 							}
-							
+
 							@Override
 							public void onFailure(Throwable caught) {
 								// TODO Auto-generated method stub
-								
+
 							}
 						});
 			}
 		});
 	}
-	
+
 }
