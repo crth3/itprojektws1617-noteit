e2d31ac94d81ff1a158667dd2637d13d17b23349
ChristianUlrichRoth <cr078@hdm-stuttgart.de>
2017-01-23 18:55:17 +0100

    cleanup

--- noteit/src/de/hdm/itprojekt/noteit/client/NotesReport.java
+++ noteit/src/de/hdm/itprojekt/noteit/client/NotesReport.java
@@ -1,10 +1,6 @@
 package de.hdm.itprojekt.noteit.client;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 
@@ -14,35 +10,14 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-import de.hdm.itprojekt.noteit.shared.NotesAdministrationAsync;
-import de.hdm.itprojekt.noteit.shared.ReportService;
-import de.hdm.itprojekt.noteit.shared.ReportServiceAsync;
-
-
 /**
  * 
- * @Entry point classes define <code>onModuleLoad()</code>.
- *s
+ * @Entry point classes define <code>onModuleLoad()</code>. s
  */
 public class NotesReport implements EntryPoint {
 
-	
-	private static Logger rootLogger = Logger.getLogger("");
-
-	
-	/**
-	 * Create a remote service proxy to talk to the server-side Report
-	 * service.
-	 */
-	private final ReportServiceAsync report = ClientsideSettings.getReportService();
-	private NotesAdministrationAsync notesAdministration = null; 
-
-	//Logger
-	private static Logger logger = Logger.getLogger("");
-
-	private HorizontalPanel headlinePanel = new HorizontalPanel();
+	HorizontalPanel headlinePanel = new HorizontalPanel();
 	HorizontalPanel headerPanel = new HorizontalPanel();
-	private VerticalPanel vpReport = new VerticalPanel();
 	HorizontalPanel contentPanel = new HorizontalPanel();
 	HorizontalPanel navPanel = new HorizontalPanel();
 	VerticalPanel vpLeft = new VerticalPanel();
@@ -51,11 +26,15 @@ public class NotesReport implements EntryPoint {
 	Button bGeneral = new Button("Allgemeine Informationen zu Notizen");
 	Button bSharing = new Button("Informationen zu Notizen und Berechtigungen eines Nutzers");
 	Label headlineLabel = new Label("NoteIt Report");
-	
+
 	@Override
+	/**
+	 * Jedes GWT Widget wird mit der onModuleLoad- Methode aufgerufen. Sie gibt
+	 * an, was geschehen soll, wenn eine Widget-Instanz zur Anzeige gebracht
+	 * wird.
+	 */
 	public void onModuleLoad() {
-		 		
-		
+
 		/*-------------Styles -------------*/
 
 		headerPanel.setStylePrimaryName("headerPanel");
@@ -64,9 +43,8 @@ public class NotesReport implements EntryPoint {
 		contentPanel.setStylePrimaryName("contentPanel");
 		navPanel.setStylePrimaryName("navPanel");
 
-		
 		/*-----------------Width-------------*/
-		
+
 		navPanel.setWidth("100%");
 		vpBasisPanel.setWidth("100%");
 		headlinePanel.setWidth("100%");
@@ -74,7 +52,7 @@ public class NotesReport implements EntryPoint {
 		contentPanel.setWidth("100%");
 		bGeneral.setWidth("100%");
 		bSharing.setWidth("100%");
-		
+
 		/*-------------Widgets-------------*/
 
 		headlinePanel.add(headlineLabel);
@@ -84,30 +62,37 @@ public class NotesReport implements EntryPoint {
 		vpBasisPanel.add(contentPanel);
 		navPanel.add(bGeneral);
 		navPanel.add(bSharing);
-			
+
 		RootPanel.get("Reporthead").add(vpBasisPanel);
-		
+
+		/**
+		 * Button-SharingINformation, der die benötigte View zur Verfügung
+		 * stellt
+		 */
 		bSharing.addClickHandler(new ClickHandler() {
-		public void onClick(ClickEvent event) {
-			
-			contentPanel.clear();
-			NotesSharingInformationReport notesSharingInformation = new NotesSharingInformationReport();
-			contentPanel.add(notesSharingInformation);
-			
+			public void onClick(ClickEvent event) {
+
+				contentPanel.clear();
+				NotesSharingInformationReport notesSharingInformation = new NotesSharingInformationReport();
+				contentPanel.add(notesSharingInformation);
+
 			}
-		
-	});
-		
+
+		});
+
+		/**
+		 * Button-GeneralInformation, der die benötigte View zur Verfügung
+		 * stellt
+		 */
 		bGeneral.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				
+
 				contentPanel.clear();
 				NotesGeneralInformationReport notesGeneralInformation = new NotesGeneralInformationReport();
 				contentPanel.add(notesGeneralInformation);
-				}
-			
+			}
+
 		});
-			
-	
-		}
+
 	}
+}
